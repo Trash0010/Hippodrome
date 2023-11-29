900d2c5f8794461daa098df8f3bb2bdf9d15049c
Sasha <chartorijskij@gmail.com>
2023-11-29 21:07:52 +0200

    add test for class Main

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    @Disabled
+    @Timeout(value = 22, unit = TimeUnit.SECONDS)
+    public void mainMethodTakesNoLongerThanTwentyTwoSeconds() throws Exception {
+        Main.main(new String[]{});
+    }
+}
